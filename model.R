5f430db88bfccdd8c5b926bc99e8610f92b543b4
Aditya Singh <ad1tyx@icloud.com>
2025-07-19 10:47:25 +0530

    bsopm: Initial pricer
    leverage d1 and d2 to compute call and put prices

--- model.R
+++ model.R
@@ -4,5 +4,19 @@ compute_d1_d2 <- function(S0, K, T, r, sigma) {
   list(d1 = d1, d2 = d2)
 }
 
-# Quick test
-print(compute_d1_d2(S0 = 100, K = 100, T = 1, r = 0.05, sigma = 0.2))
+bs_price <- function(type, S0, K, T, r, sigma) {
+  ds <- compute_d1_d2(S0, K, T, r, sigma)
+  d1 <- ds$d1; d2 <- ds$d2
+
+  if (tolower(type) == "call") {
+    S0 * pnorm(d1) - K * exp(-r * T) * pnorm(d2)
+  } else if (tolower(type) == "put") {
+    K * exp(-r * T) * pnorm(-d2) - S0 * pnorm(-d1)
+  } else {
+    stop("Option type must be 'call' or 'put'.")
+  }
+}
+
+# Quick tests:
+cat("Call:", bs_price("call", 100, 100, 1, 0.05, 0.2), "\n")
+cat("Put: ", bs_price("put",  100, 100, 1, 0.05, 0.2), "\n")
